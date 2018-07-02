9658d5565e35843924391c4f0f1ffc6c277144a1
Zack Kollar <rallokkcaz@gmail.com>
2018-07-02 01:32:39 -0700

    Testing configuration

--- parcel/lib/entry_point.py
+++ parcel/lib/entry_point.py
@@ -1,4 +1,25 @@
-import threading
+import time
+import atexit
+from threading import Thread
+
+
+watching = True
+
+def watch():
+    global watching
+    while watching:
+        time.sleep(0.5)
+
+watch_thread = Thread(target=watch)
+
+@atexit.register
+def stop():
+    print('Parcel is shutting down...')
+    global watching
+    watching = False
+    watch_thread.join()
+
 
 def start_parcel():
     print('Parcel is waiting...')
+    watch_thread.start()
