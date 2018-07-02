222a7b5ca050ea6f914733a07680c7f9ef2606cd
Zack Kollar <rallokkcaz@gmail.com>
2018-07-02 01:37:55 -0700

    Working on work arounds for watching files

--- parcel/lib/entry_point.py
+++ parcel/lib/entry_point.py
@@ -12,14 +12,17 @@ def watch():
 
 watch_thread = Thread(target=watch)
 
-@atexit.register
+
 def stop():
-    print('Parcel is shutting down...')
     global watching
+    global watch_thread
+
+    print('Parcel is shutting down...')
     watching = False
     watch_thread.join()
 
 
 def start_parcel():
+    atexit.register(stop)
     print('Parcel is waiting...')
     watch_thread.start()
